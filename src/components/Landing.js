8e26372b18d341b77ec92d31140f230325299d80
Bealonzo <alonzo.briane@gmail.com>
2019-02-19 12:52:56 -0500

    Complete-css

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,14 +7,17 @@ const Landing =() => (
   <section className="selling-points">
     <div className="point">
       <h2 className="point-title">Choose your music</h2>
+      				<img className="point-image"  src="assets/images/record1.jpg" id="PtImg1" alt=" Broken record"></img>
       <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
     </div>
     <div className="point">
       <h2 className="point-title">Unlimited, streaming, ad-free</h2>
+    				<img className="point-image" src="assets/images/music.jpg" id="PtImg2" alt="emblem with music writen in the center"></img>
       <p className="point-description">No arbitrary limits. No distractions.</p>
     </div>
     <div className="point">
       <h2 className="point-title">Mobile enabled</h2>
+            <img className="point-image"  src="assets/images/man.jpg" id="PtImg3" alt="man in a montain with earbud on"></img>
       <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
     </div>
   </section>
